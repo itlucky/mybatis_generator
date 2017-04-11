d484a9d0aafa4e43ea05169505e6f17fc6b8bb3f
jayHon <1549700290@qq.com>
2017-04-11 16:28:34 +0800

    mybatis逆向工程：配置和生成程序参考官网即可

---
+++ src/cn/hjie/ssm/main/GeneratorSqlMap.java
@@ -0,0 +1,43 @@
+package cn.hjie.ssm.main;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * User : Hong.jie
+ * Date : 2017/4/11
+ * Time : 15:45
+ * Created with 集团三码项目组
+ * Annotation : mybatis逆向工程的 生成程序
+ *  官网有代码实例-- http://www.mybatis.org/generator/running/runningWithJava.html
+ */
+public class GeneratorSqlMap {
+    public void generator() throws Exception{
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        //复制官网代码，需要修改的地方就是这里指定生成的配置文件
+        File configFile = new File("generatorConfig.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+                callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+
+    public static void main(String[] args) throws Exception {
+        try {
+            GeneratorSqlMap generatorSqlmap = new GeneratorSqlMap();
+            generatorSqlmap.generator();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
